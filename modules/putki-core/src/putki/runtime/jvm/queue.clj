21f33da3ab27632f26127ca02d1ce292c173a0b4
Toni Vanhala <toni.vanhala@metosin.fi>
2020-10-03 08:40:22 +0300

    feat(jvm): add updateable priority queue
    Add priority queue that supports updating the
    priority of the items it holds.

--- modules/putki-core/src/putki/runtime/jvm/queue.clj
+++ modules/putki-core/src/putki/runtime/jvm/queue.clj
@@ -1,7 +1,9 @@
 (ns putki.runtime.jvm.queue
-  (:import (java.util.concurrent ArrayBlockingQueue)))
+  (:import (java.util.concurrent ArrayBlockingQueue PriorityBlockingQueue ConcurrentHashMap)
+           (java.util Comparator)))
 
 (def ^:private +default-fifo-queue-size+ 32)
+(def ^:private +default-priority-queue-size+ 11)
 
 (defprotocol
   DataQueue
@@ -32,3 +34,71 @@
        clojure.lang.Counted
        (count [_this]
          (.size queue))))))
+
+(defn updateable-priority-queue
+  ([]
+   (updateable-priority-queue {}))
+  ([{:keys [get-id get-priority initial-size replace?]
+     :or {get-id :id
+          get-priority :priority
+          initial-size +default-priority-queue-size+
+          replace? (fn [_current-item _new-item] true)}}]
+   (let [-comparator (reify Comparator
+                       (compare [_this one other]
+                         (let [first-prio (-> one :item get-priority long)
+                               second-prio (-> other :item get-priority long)]
+                           (.compareTo (Long/valueOf first-prio) (Long/valueOf second-prio))))
+                       (equals [this other]
+                         (.equals this other)))
+         queue (PriorityBlockingQueue. initial-size -comparator)
+         valid-item-by-id (ConcurrentHashMap.)
+         mutex (Object.)
+         valid-item? (fn [item]
+                       (let [id (some-> item :item get-id)
+                             priority (some-> item :item get-priority)]
+                         (locking mutex
+                           (and
+                             id
+                             priority
+                             (= priority (some->> id
+                                                  (.get valid-item-by-id)
+                                                  get-priority))))))]
+     (reify
+       DataQueue
+       (put-item [_this item]
+         (locking mutex
+           (let [id (get-id item)
+                 current-item (.get valid-item-by-id id)]
+             (when (or (nil? current-item) (replace? current-item item))
+               (.put valid-item-by-id id item)
+               (.put queue {:item item})))))
+       (peek-item [_this]
+         (locking mutex
+           (loop [item (.peek queue)]
+             (cond
+               (valid-item? item)
+               (:item item)
+               item
+               (do (.poll queue)
+                   (recur (.peek queue)))
+               :else
+               ::nil))))
+       (pop-item [_this]
+         (locking mutex
+           (loop [item (.poll queue)]
+             (cond
+               (valid-item? item)
+               (do
+                 (.remove valid-item-by-id (-> item :item get-id))
+                 (:item item))
+               item
+               (recur (.poll queue))
+               :else
+               ::nil))))
+       (clear-queue [_this]
+         (locking mutex
+           (.clear queue)
+           (.clear valid-item-by-id)))
+       clojure.lang.Counted
+       (count [_this]
+         (.size valid-item-by-id))))))
--- test/putki/runtime/jvm/queue_test.clj
+++ test/putki/runtime/jvm/queue_test.clj
@@ -24,3 +24,76 @@
       (is (= 0 (count queue)))
       (is (= ::q/nil (q/peek-item queue)))
       (is (= ::q/nil (q/pop-item queue))))))
+
+(deftest updateable-priority-queue-test
+  (testing "items are returned in priority order"
+    (let [queue (q/updateable-priority-queue)
+          priorities [42 313 60 0 100]
+          expected (sort priorities)
+          items (for [p priorities]
+                  {:id (str "foo_" p)
+                   :priority p})]
+      (doseq [item items]
+        (q/put-item queue item))
+      (doseq [e expected]
+        (is (= {:id (str "foo_" e) :priority e} (q/pop-item queue))))
+      (is (= ::q/nil (q/pop-item queue)))))
+  (testing "items can be updated with new priority"
+    (let [queue (q/updateable-priority-queue {:get-priority #(- (:priority %))})
+          priorities [39 37 400 -5 100 90 1000 1001 1002 1003 1004 1005 1006]
+          expected (sort-by - priorities)
+          items (for [p priorities]
+                  {:id (str "bar " p)
+                   :priority p})]
+      (doseq [item items]
+        (q/put-item queue item))
+      (is (= 13 (count queue)))
+      (doseq [e (take 7 expected)]
+        (is (= {:id (str "bar " e)
+                :priority e}
+               (q/pop-item queue))))
+      (is (= 6 (count queue)))
+      (q/put-item queue {:id "bar 37"
+                         :priority 999})
+      (q/put-item queue {:id "bar 400"
+                         :priority 1})
+      (is (= 6 (count queue)))
+      (doseq [e [{:id "bar 37" :priority 999}
+                 {:id "bar 100" :priority 100}
+                 {:id "bar 90" :priority 90}
+                 {:id "bar 39" :priority 39}
+                 {:id "bar 400" :priority 1}
+                 {:id "bar -5" :priority -5}]]
+        (is (= e (q/pop-item queue))))
+      (is (= ::q/nil (q/pop-item queue)))))
+  (testing "can reinsert item with same id"
+    (let [queue (q/updateable-priority-queue)]
+      (doseq [item [{:id :foo :priority 45}
+                    {:id :bar :priority 35}
+                    {:id :baz :priority 66}]]
+        (q/put-item queue item))
+      (is (= {:id :bar :priority 35} (q/pop-item queue)))
+      (is (= {:id :foo :priority 45} (q/pop-item queue)))
+      (doseq [item [{:id :foo :priority 45}
+                    {:id :bar :priority 35}
+                    {:id :baz :priority 66}]]
+        (q/put-item queue item))
+      (is (= {:id :bar :priority 35} (q/pop-item queue)))
+      (is (= {:id :foo :priority 45} (q/pop-item queue)))
+      (is (= {:id :baz :priority 66} (q/pop-item queue)))
+      (is (= ::q/nil (q/pop-item queue)))))
+  (testing "custom selector fn for priority"
+    (let [ids (range 20)
+          prios (map #(- 10 %) ids)
+          get-priority :real-priority
+          queue (q/updateable-priority-queue {:get-priority get-priority})]
+      (doseq [[id prio] (map vector ids prios)]
+        (q/put-item queue {:id id :priority id :real-priority prio}))
+      (doseq [expected (sort prios)]
+        (is (= expected (some-> (q/pop-item queue) get-priority))))
+      (is (= ::q/nil (q/peek-item queue)))))
+  (testing "custom predicate fn for replacing item"
+    (let [replace? (fn [current-item new-item]
+                     (< (:priority current-item) (:priority new-item)))
+          queue (q/updateable-priority-queue {:replace? replace?})]))
+  (testing "can clear and reuse queue"))
